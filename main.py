d7329fda4e28d8f57384d017ad3f8306c05973b7
Quantumela <hello@quantumela.com>
2025-07-16 02:54:35 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #helloninja
+import streamlit as st
+
+st.set_page_config(page_title="Hello World", layout="centered")
+
+st.title("👋 Hello, Streamlit!")
+st.write("This is a test to make sure your Streamlit app is running.")
